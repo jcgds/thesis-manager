a48262dcddc1e9f47b56d3de8cb7db613fcc71af
Juan Goncalves <juan.goncalves498@gmail.com>
2020-01-05 16:18:51 -0400

    Create the Proposal model

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User, PersonData
+
+from .models import *
 
 admin.site.register(User)
 admin.site.register(PersonData)
+admin.site.register(Proposal)
--- web/models.py
+++ web/models.py
@@ -20,11 +20,24 @@ class PersonData(models.Model):
     id_card_number = models.CharField(max_length=16, unique=True)  # Cédula
     name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
-    ucab_email = models.EmailField(unique=True)
+    ucab_email = models.EmailField(unique=True, null=True)
     email = models.EmailField()
     primary_phone_number = PhoneNumberField()
-    secondary_phone_number = PhoneNumberField()
-    observations = models.CharField(max_length=1_024, default='')
+    secondary_phone_number = PhoneNumberField(null=True)
+    observations = models.CharField(max_length=1_024, null=True)
 
     class Meta:
         verbose_name_plural = 'Person data'
+
+
+class Proposal(models.Model):
+    code = models.CharField(max_length=64)
+    submission_date = models.DateField()
+    title = models.CharField(max_length=256)
+    student1 = models.ForeignKey(PersonData, models.PROTECT, related_name='student1')
+    student2 = models.ForeignKey(PersonData, models.PROTECT, null=True, related_name='student2')
+    academic_tutor = models.ForeignKey(PersonData, models.PROTECT, related_name='academic_tutor')
+    industry_tutor = models.ForeignKey(PersonData, models.PROTECT, null=True, related_name='industry_tutor')
+    # TODO: Make custom validator (201915 - 201925 etc) [Unit testable]
+    # TODO: Maybe move to its own table
+    term = models.CharField(max_length=6)
