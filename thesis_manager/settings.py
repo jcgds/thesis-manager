22fe90cacc66e35d06b8bfa8f1e51e19efb6b101
Juan Goncalves <juan.goncalves498@gmail.com>
2020-01-05 15:20:57 -0400

    Create the User and PersonData models

--- thesis_manager/settings.py
+++ thesis_manager/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'web',
+    'phonenumber_field',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -81,6 +83,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'web.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
+PHONENUMBER_DEFAULT_REGION = 'US'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
--- web/models.py
+++ web/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_manager = models.BooleanField(default=False)
+
+
+class PersonData(models.Model):
+    TEACHER = 0
+    STUDENT = 1
+    EXTERNAL = 2
+    TYPE_CHOICES = (
+        (TEACHER, 'Teacher'),
+        (STUDENT, 'Student'),
+        (EXTERNAL, 'External')
+    )
+    type = models.PositiveSmallIntegerField(choices=TYPE_CHOICES)
+    id_card_number = models.CharField(max_length=16, unique=True)  # Cédula
+    name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    ucab_email = models.EmailField(unique=True)
+    email = models.EmailField()
+    primary_phone_number = PhoneNumberField()
+    secondary_phone_number = PhoneNumberField()
+    observations = models.CharField(max_length=1_024, default='')
+
+    class Meta:
+        verbose_name_plural = 'Person data'
