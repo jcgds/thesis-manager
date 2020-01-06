f0b3561e76fef9438084ea82b1de8af3bffed12c
Juan Goncalves <juan.goncalves498@gmail.com>
2020-01-06 11:45:09 -0400

    Configure person detail view

--- web/views.py
+++ web/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import PersonData
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the web index.")
+
+
+def person_detail(request, person_id_card_number):
+    person = get_object_or_404(PersonData, id_card_number=person_id_card_number)
+    context = {
+        'person_data': person
+    }
+    return render(request, 'web/person_detail.html', context)
