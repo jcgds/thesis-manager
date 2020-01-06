1ec7dec48587715b9a8314690251b0abe80d4737
Juan Goncalves <juan.goncalves498@gmail.com>
2020-01-06 10:14:13 -0400

    Create command to generate fake Persons for development

---
+++ web/management/commands/fakepersons.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+from web.models import PersonData
+
+
+class Command(BaseCommand):
+    help = 'Generates and inserts fake data into the database'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        loc_faker = Faker(['es_ES', 'es_MX', 'en_US'])
+        faker = Faker()
+        id_types = ['V', 'E']
+        for i in range(20):
+            PersonData(
+                # If the types are modified in the PersonData model, it should be updated here too
+                type=loc_faker.random_int(0, 2),
+                id_card_number='%s%d' % (faker.random.choice(id_types), loc_faker.random_int(1_000_000, 30_000_000)),
+                name=loc_faker.first_name(),
+                last_name=loc_faker.last_name(),
+                ucab_email=loc_faker.company_email(),
+                email=loc_faker.email(),
+                primary_phone_number=loc_faker.phone_number()
+            ).save()
+        self.stdout.write(self.style.SUCCESS('Successfully created Persons'))
