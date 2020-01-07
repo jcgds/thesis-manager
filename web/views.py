8a12c81b014a8bff255e53f01aedbbcf5df903dc
Juan Goncalves <juan.goncalves498@gmail.com>
2020-01-07 15:23:32 -0400

    Improve searches by crosschecking the terms for every applicable column

--- web/views.py
+++ web/views.py
@@ -1,3 +1,6 @@
+import operator
+from functools import reduce
+
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import HttpResponse
@@ -20,15 +23,22 @@ def person_detail(request, person_id_card_number):
 
 
 def person_index(request):
-    id_or_name_filter = request.GET.get('search')
-    query = Q(id_card_number__icontains=id_or_name_filter) \
-            | Q(name__icontains=id_or_name_filter) \
-            | Q(last_name__icontains=id_or_name_filter)
-    person_list = PersonData.objects.filter(query) if id_or_name_filter else PersonData.objects.all()
+    search_param = request.GET.get('search')
+    if search_param:
+        # Append a query for each term received in the search parameters so that if we receive multiple
+        # parameters, we crosscheck every single one with the colums id_card_number, name and last_name
+        search_args = []
+        for term in search_param.split():
+            for query in ('id_card_number__icontains', 'name__icontains', 'last_name__icontains'):
+                search_args.append(Q(**{query: term}))
+        person_list = PersonData.objects.filter(reduce(operator.or_, search_args))
+    else:
+        # If we don't receive a search parameter, don't apply any filters
+        person_list = PersonData.objects.all()
+
     paginator = Paginator(person_list, request.GET.get('page_length', 15))
     page = request.GET.get('page')
     persons_by_page = paginator.get_page(page)
-    search_param = request.GET.get('search')
     context = {
         'person_list': persons_by_page,
         'search_form': SearchForm(previous_search=search_param),
