89894829b8f25a491399e152485ed7bea0ad97dd
Juan Goncalves <juan.goncalves498@gmail.com>
2020-01-07 00:29:55 -0400

    Basic person list implementation

--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('personas/<str:person_id_card_number>', views.person_detail, name='person_detail')
+    path('personas', views.person_index, name='person_index'),
+    path('personas/<str:person_id_card_number>', views.person_detail, name='person_detail'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
@@ -14,3 +16,18 @@ def person_detail(request, person_id_card_number):
         'person_data': person
     }
     return render(request, 'web/person_detail.html', context)
+
+
+def person_index(request):
+    id_or_name_filter = request.GET.get('search')
+    query = Q(id_card_number__contains=id_or_name_filter) \
+            | Q(name__contains=id_or_name_filter) \
+            | Q(last_name__contains=id_or_name_filter)
+    person_list = PersonData.objects.filter(query) if id_or_name_filter else PersonData.objects.all()
+    paginator = Paginator(person_list, request.GET.get('page_length', 15))
+    page = request.GET.get('page')
+    persons_by_page = paginator.get_page(page)
+    context = {
+        'person_list': persons_by_page
+    }
+    return render(request, 'web/person_list.html', context)
