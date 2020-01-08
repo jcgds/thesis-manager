ccbaa6f1c76938fb5f90cf199948dc457282f034
Juan Goncalves <juan.goncalves498@gmail.com>
2020-01-07 22:23:53 -0400

    Fix auto wrapping

--- web/management/commands/fakepersons.py
+++ web/management/commands/fakepersons.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from faker import Faker
 
-from web.models import PersonData
+from web.models import PersonData, PersonType
 
 
 class Command(BaseCommand):
@@ -14,10 +14,11 @@ class Command(BaseCommand):
         loc_faker = Faker(['es_ES', 'es_MX', 'en_US'])
         faker = Faker()
         id_types = ['V', 'E']
+        person_types = PersonType.objects.all()
         for i in range(20):
             PersonData(
                 # If the types are modified in the PersonData model, it should be updated here too
-                type=loc_faker.random_int(0, 2),
+                type=faker.random.choice(list(person_types)),
                 id_card_number='%s%d' % (faker.random.choice(id_types), loc_faker.random_int(1_000_000, 30_000_000)),
                 name=loc_faker.first_name(),
                 last_name=loc_faker.last_name(),
