076b33ce45b93c55ed18f892c26d7e1c3424e0b1
Fernando Consalvo <fconsalvo97@gmail.com>
2020-01-08 15:35:50 -0400

    Add commands to generate data for thesis and proposals

---
+++ web/management/commands/fakeproposal.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+from web.models import PersonData, Proposal, ProposalStatus, PersonType, Term
+
+
+class Command(BaseCommand):
+    help = 'Generates and inserts fake data into the database'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        loc_faker = Faker(['es_ES', 'es_MX', 'en_US'])
+        faker = Faker()
+        proposal_statuses = ProposalStatus.objects.all()
+        for i in range(6):
+            Proposal(
+                code=loc_faker.random_int(),
+                submission_date=loc_faker.date_time_between(start_date='-6m', end_date='now'),
+                title=loc_faker.sentence(nb_words=6),
+                student1=faker.random.choice(PersonData.objects.filter(type=PersonType.objects.get(name='Estudiante').id)),
+                academic_tutor=faker.random.choice(PersonData.objects.filter(type=PersonType.objects.get(name='Profesor').id)),
+                industry_tutor=faker.random.choice(PersonData.objects.filter(type=PersonType.objects.get(name='Externo').id)),
+                term=faker.random.choice(Term.objects.all()),
+            ).save()
+        self.stdout.write(self.style.SUCCESS('Successfully created Proposals'))
