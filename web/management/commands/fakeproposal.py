aeb4c6ef2a0ca334e4bf6a2ce1a4fc7be3c26d10
Fernando Consalvo <fconsalvo97@gmail.com>
2020-01-09 23:07:00 -0400

    Fix fakeproposal

--- web/management/commands/fakeproposal.py
+++ web/management/commands/fakeproposal.py
@@ -14,11 +14,18 @@ class Command(BaseCommand):
         loc_faker = Faker(['es_ES', 'es_MX', 'en_US'])
         faker = Faker()
         proposal_statuses = ProposalStatus.objects.all()
+        terms = Term.objects.all()
+        if not terms:
+            Term(
+                period='marzo-julio'
+            ).save()
         for i in range(6):
+
             Proposal(
                 code=loc_faker.random_int(),
                 submission_date=loc_faker.date_time_between(start_date='-1y', end_date='now'),
                 title=loc_faker.sentence(nb_words=6),
+                proposal_status=faker.random.choice(proposal_statuses),
                 student1=faker.random.choice(PersonData.objects.filter(type=PersonType.objects.get(name='Estudiante').id)),
                 academic_tutor=faker.random.choice(PersonData.objects.filter(type=PersonType.objects.get(name='Profesor').id)),
                 industry_tutor=faker.random.choice(PersonData.objects.filter(type=PersonType.objects.get(name='Externo').id)),
